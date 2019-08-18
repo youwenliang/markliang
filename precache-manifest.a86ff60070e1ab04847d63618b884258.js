e41bee6883ccba1c71bab749790981b54bd3c27c
youwenliang <mliang@mozilla.com>
2019-08-18 12:08:16 +0800

    Updates

---
+++ precache-manifest.a86ff60070e1ab04847d63618b884258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3577bdd039ec9792196cd79461ff5a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a49ec6d8eccbcaa62b",
+    "url": "/static/css/main.5b0dc0e1.chunk.css"
+  },
+  {
+    "revision": "7cab29a1aa59e81c7f25",
+    "url": "/static/js/2.54720bdb.chunk.js"
+  },
+  {
+    "revision": "00a49ec6d8eccbcaa62b",
+    "url": "/static/js/main.fcae5b8a.chunk.js"
+  },
+  {
+    "revision": "97d78d26826bf7c68729",
+    "url": "/static/js/runtime~main.91d9f00c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
