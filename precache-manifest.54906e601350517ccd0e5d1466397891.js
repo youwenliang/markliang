0a8fd5f0a733cb90212d9af86f0232e5b66f99d6
System Administrator <root@Marks-MacBook-Pro.local>
2020-08-19 23:35:41 +0800

    Updates

---
+++ precache-manifest.54906e601350517ccd0e5d1466397891.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0010d2cd0a8466678b3ca9461d339e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e19b3dbdb516fbbd37e",
+    "url": "/static/css/main.259198eb.chunk.css"
+  },
+  {
+    "revision": "4659d9b63f726ff0609a",
+    "url": "/static/js/2.0de6ae48.chunk.js"
+  },
+  {
+    "revision": "2e19b3dbdb516fbbd37e",
+    "url": "/static/js/main.bbe2e14d.chunk.js"
+  },
+  {
+    "revision": "97d78d26826bf7c68729",
+    "url": "/static/js/runtime~main.91d9f00c.js"
+  },
+  {
+    "revision": "6021dfe8dec0b7e6578d6acb473c0121",
+    "url": "/static/media/AirbnbCerealBold.6021dfe8.woff"
+  },
+  {
+    "revision": "886339361d3ff50e8a95ac5cdc63a82e",
+    "url": "/static/media/AirbnbCerealBook.88633936.woff"
+  },
+  {
+    "revision": "d2a616df336ba4fbc0bafbf4683da4b1",
+    "url": "/static/media/AirbnbCerealLight.d2a616df.woff"
+  },
+  {
+    "revision": "220b1dd31cbf1b11825433158d644def",
+    "url": "/static/media/AirbnbCerealMedium.220b1dd3.woff"
+  },
+  {
+    "revision": "29f2d27a132c99016332fff4dc264abb",
+    "url": "/static/media/icon0.29f2d27a.svg"
+  },
+  {
+    "revision": "b58de5eb12f504d9d7132d053f284666",
+    "url": "/static/media/icon1.b58de5eb.svg"
+  },
+  {
+    "revision": "d65c8570c47214bb7405def38a434b09",
+    "url": "/static/media/icon2.d65c8570.svg"
+  },
+  {
+    "revision": "612577e195df150f88c2e56b803735fc",
+    "url": "/static/media/icon3.612577e1.svg"
+  },
+  {
+    "revision": "acc6b1fa54a47b3356cf54d480907f31",
+    "url": "/static/media/icon4.acc6b1fa.svg"
+  },
+  {
+    "revision": "18ffbca409fd5026d7cb6ff46326ef22",
+    "url": "/static/media/illustration1.18ffbca4.svg"
+  },
+  {
+    "revision": "4f2be23e98412ec88655af9867a8e6bc",
+    "url": "/static/media/illustration2.4f2be23e.svg"
+  },
+  {
+    "revision": "72f576873bdc63cd492e8a703e2b959c",
+    "url": "/static/media/illustration3.72f57687.svg"
+  },
+  {
+    "revision": "45d749c55a306358a5b70558eac787a3",
+    "url": "/static/media/illustration4.45d749c5.svg"
+  },
+  {
+    "revision": "6ad7d956bac1b1c990941cd2670f299f",
+    "url": "/static/media/illustration5.6ad7d956.svg"
+  },
+  {
+    "revision": "0655f677c7e6c3f5e7b13239ebba5ca8",
+    "url": "/static/media/illustration6.0655f677.svg"
+  },
+  {
+    "revision": "37f2e4a9978a6deb3e8607c394ae81b9",
+    "url": "/static/media/logo.37f2e4a9.svg"
+  },
+  {
+    "revision": "d607bcb1461ae05f2e30d1d6e7b19095",
+    "url": "/static/media/photo1.d607bcb1.jpg"
+  },
+  {
+    "revision": "a7c199245d80fd07cadbce0246e581ea",
+    "url": "/static/media/photo2.a7c19924.jpg"
+  },
+  {
+    "revision": "15cd7e832493373a6a3758eb6d7e7323",
+    "url": "/static/media/photo3.15cd7e83.jpg"
+  },
+  {
+    "revision": "ce5b78a366065e7ab9dc7f3470c784e0",
+    "url": "/static/media/photo4.ce5b78a3.jpg"
+  },
+  {
+    "revision": "20fa39cda999074c8c2b9d7f4d6eafd1",
+    "url": "/static/media/photo5.20fa39cd.JPG"
+  },
+  {
+    "revision": "c4a0b69bce00672db5b1a90846690d11",
+    "url": "/static/media/photo6.c4a0b69b.JPG"
+  },
+  {
+    "revision": "31df739de7ec11e88f11e604a322a802",
+    "url": "/static/media/photo7.31df739d.jpg"
+  },
+  {
+    "revision": "40575c1a9defb8e8a84a90d6ad95cb26",
+    "url": "/static/media/travel01.40575c1a.jpg"
+  },
+  {
+    "revision": "74ee02240a64a1b38daec2c772828659",
+    "url": "/static/media/travel02.74ee0224.jpg"
+  },
+  {
+    "revision": "073998ea3f325d10cbc68a2bd4850a23",
+    "url": "/static/media/travel03.073998ea.jpg"
+  },
+  {
+    "revision": "6a2b7c757386958a67ed0259f33a26ee",
+    "url": "/static/media/travel04.6a2b7c75.jpg"
+  },
+  {
+    "revision": "bd9160c81376327681b7867ee40d47d9",
+    "url": "/static/media/travel05.bd9160c8.jpg"
+  },
+  {
+    "revision": "bb2e36abd9f0077540160c87f9f8c9ca",
+    "url": "/static/media/travel06.bb2e36ab.jpg"
+  },
+  {
+    "revision": "c9290c888d033a18939914ff4c396b8d",
+    "url": "/static/media/travel07.c9290c88.jpg"
+  },
+  {
+    "revision": "21adb756761ce9b65476993521c249dd",
+    "url": "/static/media/travel08.21adb756.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bcb19fda9bf4faeff39b6c2a71f287ad.js"
+  "/precache-manifest.54906e601350517ccd0e5d1466397891.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
